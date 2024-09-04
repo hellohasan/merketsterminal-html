86761480ed051480657814b5f378d90460055d28
juelthpi <juelcse2019@gmail.com>
2024-09-05 00:52:07 +0600

    mega menu update, country code update, scrollbar master add .......

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,56 @@
 jQuery(document).ready(function ($) {
    
   // menu bg add
-  $(window).on("scroll", function () {
-    if ($(window).scrollTop() >50) {
-      $("header").addClass("menu-bg");
-    } else {
-      $("header").removeClass("menu-bg");
-    }
-  });
+  // $(window).on("scroll", function () {
+  //   if ($(window).scrollTop() >50) {
+  //     $("header").addClass("menu-bg");
+  //   } else {
+  //     $("header").removeClass("menu-bg");
+  //   }
+  // });
+  // $(".close-mark").click(function(){
+  //   $(".country-menu").hide();
+  // });
   
 });
+// scroll bar js
+document.addEventListener('DOMContentLoaded', function () {
+  new SimpleBar(document.getElementById('scrollbar'));
+});
+ 
+
+
+const toggleDropdown = $(".nav-link.dropdown-toggle");
+const dropdown = $(".terget-content");
+const mainContent = $(".blur-content");
+
+toggleDropdown.click(function () {
+  dropdown.toggleClass("show");
+
+  if (dropdown.hasClass("show")) {
+    mainContent.css({
+      "-webkit-filter": "blur(5px)",
+      "-moz-filter": "blur(5px)",
+      "-o-filter": "blur(5px)",
+      "-ms-filter": "blur(5px)",
+      filter: "blur(5px)",
+      
+      
+    });
+  } else {
+    mainContent.css({
+      "-webkit-filter": "none",
+      "-moz-filter": "none",
+      "-o-filter": "none",
+      "-ms-filter": "none",
+      filter: "none"
+    });
+  }
+});
+
 document.addEventListener("click", function (event) {
   const div = document.getElementById("outside-click");
-  const navbarText = document.getElementById("navbarText");
+  const navbarText = document.getElementById("main_nav");
   if (!div.contains(event.target)) {
     // console.log('Clicked outside the div');
     navbarText.classList.remove("show");
